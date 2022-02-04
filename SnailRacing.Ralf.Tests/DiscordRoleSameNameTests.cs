3def3fcdfc03474538d427af61736d6e4a93d919
Lawrence Rabbets <lawrab@users.noreply.github.com>
2022-02-04 09:01:11 +0000

    Adding comments to clarify use of reflection

--- SnailRacing.Ralf.Tests/DiscordRoleSameNameTests.cs
+++ SnailRacing.Ralf.Tests/DiscordRoleSameNameTests.cs
@@ -1,7 +1,12 @@
 ﻿using DSharpPlus.Entities;
-using Moq;
 using Xunit;
 
+/// <summary>
+/// The test helper was created to allow us to use DSharpPlus Entities in unit tests.
+/// The project at this stage do not justify the effor to abstract DSharpPlus.
+/// Reflection is used to instantiate classes with internal constructors and to set
+/// property values with internal or private setters
+/// </summary>
 namespace SnailRacing.Ralf.Tests
 {
     public class DiscordRoleSameNameTests
@@ -11,9 +16,9 @@ namespace SnailRacing.Ralf.Tests
         {
             // Arrange
             var left = TestHelper.CreateInstance <DiscordRole>();
-            TestHelper.SetProperty(left, x => x.Name, "ABC");
+            TestHelper.SetProperty(left, x => x!.Name, "ABC");
             var right = TestHelper.CreateInstance<DiscordRole>();
-            TestHelper.SetProperty(right, x => x.Name, "ABC");
+            TestHelper.SetProperty(right, x => x!.Name, "ABC");
 
             var comparer = new DiscordRoleSameName();
 
